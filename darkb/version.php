4263a02953988da697120d9fe2628c272ce5f70a
netsapiensis <radu.groza@bitstone.eu>
2011-12-09 11:03:35 +0200

    section names left align

--- darkb/version.php
+++ darkb/version.php
@@ -3,5 +3,5 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version  = 2011110801;  // The current module version (Date: YYYYMMDDXX)
+$plugin->version  = 2011120901;  // The current module version (Date: YYYYMMDDXX)
 $plugin->component = 'theme_darkb';
